9a9d76c4442b577242b73db13a2f8f2d8eceee31
Matumbai Binale <binalematumbai@gmail.com>
2024-04-22 03:38:40 +0300

    Add 100-puppet_ssh_config.pp - Advanced task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Makes changes to global client configuration file
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  line    => '    PasswordAuthentication no',
+  path    => '/etc/ssh/ssh_config',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  ensure  => present,
+  line    => '    IdentityFile ~/.ssh/school',
+  path    => '/etc/ssh/ssh_config',
+  replace => true,
+}
