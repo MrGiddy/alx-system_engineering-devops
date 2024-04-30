ddc4565096713361473b151195bd6c9061a646a5
Matumbai Binale <binalematumbai@gmail.com>
2024-04-30 22:08:10 +0300

    (lb task 2): Refactor solution

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,48 +1,8 @@
 # Install and configure nginx web server
-
-exec { 'apt-update':
-  command => 'apt update',
-  path    => '/usr/bin/'
-}
-
-package {'nginx':
-  ensure          => installed,
-  install_options => ['-y'],
-}
-
-file {'404_page':
-  ensure  => file,
-  path    => '/var/www/html/custom_404.html',
-  content => "Ceci n'est pas une page\n\n",
-}
-
-file {'dflt_server_block':
-  ensure  => file,
-  path    => '/etc/nginx/sites-available/default',
-  content =>
-  'server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    add_header X-Served-By $hostname;
-
-    location / {
-        return 200 "Hello World!\n";
-    }
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    error_page 404 /custom_404.html;
-    location = /custom_404.html {
-        root /var/www/html;
-        internal;
-    }
-}',
-}
-
-service {'nginx':
-  ensure => 'running',
-  enable => true,
+exec { 'http header':
+  command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
+  provider => shell,
 }
