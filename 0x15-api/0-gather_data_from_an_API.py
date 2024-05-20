479b358044c57d5be9118da1ec4f643caeb07cb9
Matumbai Binale <binalematumbai@gmail.com>
2024-05-21 01:12:49 +0300

    (rest_api task 0): Gather progress data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines a function that gathers progress data from an API"""
+import requests
+
+
+def gather_todo_progress(id):
+    """Returns information about an employees todo list progress"""
+    # Get the employee's name
+    r = requests.get(f'https://jsonplaceholder.typicode.com/users?id={id}')
+    name = r.json()[0].get('name')
+
+    # Get the employee's todos
+    r = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={id}')
+    completed = []
+    for todo in r.json():
+        if todo.get('completed'):
+            completed.append(todo.get('title'))
+
+    total = len(r.json())
+    print(f'Employee {name} is done with tasks({len(completed)}/{total}):')
+
+    # Print titles of completed todos
+    for title in completed:
+        print('\t', title)
+
+
+if __name__ == '__main__':
+    import sys
+
+    id = int(sys.argv[1])
+    gather_todo_progress(id)
